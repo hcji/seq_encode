f7e01daaf6f2bfcc0e34f702971dc80462ba49d6
hcji <ji.hongchao@foxmail.com>
2020-10-19 09:44:32 +0800

    setup

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from setuptools import setup, find_packages
 setup(name='seq_encode',
       version='0.0.2',
       description='Convert SMILES to one hot matrix',
-      license='AGPLv3',
+      license='MIT',
       author='Ji Hongchao',
       author_email='ji.hongchao@foxmail.com',
       url='https://github.com/hcji/seq_encode',
-      packages=find_packages()
+      packages=find_packages(),
+	  classifiers=[
+	  'Development Status :: 4 - Beta',
+	  'Programming Language :: Python :: 3.6',
+	  'Programming Language :: Python :: 3.7',
+      'Programming Language :: Python :: 3.8'
+	  ]
      )
